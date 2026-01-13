4365f0a75c9bf90b8953f5c4d51568bc3f2be3fe
yosrafel <yos.kristanto@si.ukdw.ac.id>
2026-01-13 12:53:38 +0700

    [YOS] test

--- app/Models/Packages.php
+++ app/Models/Packages.php
@@ -5,12 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class Packages extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected static function booted()
     {
