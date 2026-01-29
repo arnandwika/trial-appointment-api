3a6d003d5f89eed8a5fc5077a0c6f25fdf8f2713
arnandwika <arnan.dwika@ti.ukdw.ac.id>
2026-01-29 16:41:16 +0700

    feat: bookin apiResource; fix schedule endpoint

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\CourseClassController;
 use App\Http\Controllers\TrainerController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\BookingController;
 
 //LOGIN
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -27,6 +28,7 @@ Route::apiResource('package', PackageController::class);
 Route::apiResource('course-class', CourseClassController::class);
 Route::apiResource('trainer', TrainerController::class);
 Route::apiResource('orders', OrderController::class);
-Route::apiResource('schedules', ScheduleController::class);
+Route::apiResource('schedule', ScheduleController::class);
+Route::apiResource('booking', BookingController::class);
 Route::get('orders/my-transaction/{userId}', [OrderController::class, 'myTransaction']);
 
