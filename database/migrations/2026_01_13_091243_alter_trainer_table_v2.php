c3d003414693646db7c4a2d0518084c2ac89199d
arnandwika <arnan.dwika@ti.ukdw.ac.id>
2026-03-11 16:49:50 +0700

    fix: another migration fail

--- database/migrations/2026_01_13_091243_alter_trainer_table_v2.php
+++ database/migrations/2026_01_13_091243_alter_trainer_table_v2.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('trainers', function (Blueprint $table) {
-            $table->string('class_id');
+            $table->string('class_id')->change();
         });
     }
 
